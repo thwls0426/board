d8a951bd40aff32119723e1befbf53af60b4fc05
thwls0426 <thwls950426@gmail.com>
2023-11-22 10:39:41 +0900

    commit 3
    id 값을 불러오지 못하는 오류발생 > long 을 Long 으로 고치니 실행된다!

--- demo/src/main/java/com/example/demo/controller/BoardService.java
+++ demo/src/main/java/com/example/demo/controller/BoardService.java
@@ -7,14 +7,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class BoardService {
     private final BoardRepository boardRepository;
     public void save(BoardDTO boardDTO){
+        boardDTO.setCreateTime(LocalDateTime.now()); //현재시간 출력
         log.info(boardDTO.toString());
-        Board board = boardDTO.toEntity();
+//        Board board = boardDTO.toEntity();
         boardRepository.save(boardDTO.toEntity());
     }
 }
