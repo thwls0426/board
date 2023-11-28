3ea0e66a69355511724752ae442680f5b5c54521
thwls0426 <thwls950426@gmail.com>
2023-11-28 12:52:20 +0900

    commit 13
    다운로드 까지 된다!
    댓글 사용시간 아직 출력 안된다.

---
+++ demo/src/main/java/com/example/demo/controller/FileDownloadController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+public class FileDownloadController {
+    @GetMapping("/download/{uuid}/{fileName}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String uuid,
+                                                 @PathVariable String fileName){
+        Path filePath = Paths.get("C:/Users/thwls/OneDrive/바탕 화면/" + uuid + fileName);
+
+        try{
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if(resource.exists() || resource.isReadable())
+                return ResponseEntity.ok()
+                        .header("Content-Disposition",
+                                "attachment; filename = \"" + resource.getFilename()+"\"")
+                        .body(resource);
+
+            else { //없으면 찾을 수 없음
+                return  ResponseEntity.notFound().build();
+            }
+        }catch (Exception e){ //아예 없는 경우
+            return ResponseEntity.internalServerError().build();
+
+        }
+    }
+}
